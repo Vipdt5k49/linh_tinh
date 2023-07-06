cf9083f2210d73d3efad43fdfa33a7c2929d48b6
Vipdt5k49 <117129840+Vipdt5k49@users.noreply.github.com>
2023-07-06 21:25:50 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,1 @@
-import pandas as pd
-import re
-xls= pd.ExcelFile(rf'/storage/emulated/0/Download/DCDS_BC_Thang_052023.xlsx')
-#df=pd.read_excel(xls,sheet_name='BCDanhMucDauTu_06029')
-df=xls.sheet_names
-k=[i for i in df if i.startswith('BCDanhMuc')][0]
-df=pd.read_excel(xls,sheet_name=k,usecols='a,b,d',names=['id','stock','quanity'])
-index_start=df[df.id.find('II')].index[0]
-index_end=df[df.id=='III'].index[1]
-df=df.iloc[index_start:index_end]
-print(df)
-print(index_start)
-#for i in df['id']:print(i)
-#print(df[df.id=='II'].index[0])
+token = re.search(r'field_csrf_ft" value="[a-z0-9]{10}',r.text).group()[-10:]
