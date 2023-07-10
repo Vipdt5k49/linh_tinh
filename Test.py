69ac2804cd394eded9394488c6c2db7b6e26acfd
Vipdt5k49 <117129840+Vipdt5k49@users.noreply.github.com>
2023-07-10 21:42:07 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,66 +3,96 @@ import pandas as pd
 import re
 import os
 import pathlib
+import time
 from datetime import datetime, timedelta
 PATH = pathlib.Path(__file__).parent.resolve()
 
 def check_updated_vesaf():
-    r=requests.get('https://wm.vinacapital.com/information-disclosure/')
-    token = re.search(r'field_csrf_ft" value="[a-z0-9]{10}',r.text).group()[-10:]
-    data = {'action': 'filterdiscl','data': f'txtfund=vesaf&txtcat=regular&txtnextp=1&field_csrf_ft={token}&_wp_http_referer=%2Finformation-disclosure%2F&txtkeywords=&mmyyyy=',
-    }
-
-    response = requests.post('https://wm.vinacapital.com/wp-admin/admin-ajax.php',  data=data)
-
-    url=re.search(r'http.{0,}_VESAF_FMS_FORM_WORKING_Ky-so_Thang_.{0,}xlsx',response.text).group()
-    file_new=re.search(r'.{8}_VESAF_FMS_FORM_WORKING_Ky-so_Thang_.{0,}xlsx',url).group()
+    def get_url():
+        r=requests.get('https://wm.vinacapital.com/information-disclosure/')
+        token = re.search(r'field_csrf_ft" value="[a-z0-9]{10}',r.text).group()[-10:]
+        data = {'action': 'filterdiscl','data': f'txtfund=vesaf&txtcat=regular&txtnextp=1&field_csrf_ft={token}&_wp_http_referer=%2Finformation-disclosure%2F&txtkeywords=&mmyyyy='}
+        response = requests.post('https://wm.vinacapital.com/wp-admin/admin-ajax.php',  data=data)
+        url=re.search(r'http.{0,}_VESAF_FMS_FORM_WORKING_Ky-so_Thang_.{0,}xlsx',response.text).group()
+        file_new=re.search(r'.{8}_VESAF_FMS_FORM_WORKING_Ky-so_Thang_.{0,}xlsx',url).group()
+        return url, file_new
 
     vesaf_is_updated = False
 
     for x in os.listdir(PATH):
         if 'VESAF' in x and '~' not in x:
-            if x == file_new:
+            now = datetime.now()
+            now_str = now.strftime("%Y%m")
+            file_time_str = f'{x[-9:-5]}{x[-11:-9]}'
+            if int(now_str) - int(file_time_str) < 2:
                 vesaf_is_updated = True
+                print(x)
                 break
             else:
-                os.remove(f'{PATH}/{x}')
-                response = requests.get(url)
-                open(f'{PATH}/{file_new}', "wb").write(response.content)
-                vesaf_is_updated = True
-                break
+                k = get_url()
+                url = k[0]
+                file_new = k[1]
+                if x == file_new:
+                    vesaf_is_updated = True
+                    print(file_new)
+                    break
+                else:
+                    os.remove(f'{PATH}/{x}')
+                    response = requests.get(url)
+                    open(f'{PATH}/{file_new}', "wb").write(response.content)
+                    vesaf_is_updated = True
+                    print(file_new)
+                    break
+            
     if vesaf_is_updated == False:
         response = requests.get(url)
         open(f'{PATH}/{file_new}', "wb").write(response.content)
-    print(file_new)
+        print(file_new)
+
 
 def check_updated_dcds():
     dcds_is_updated = False
-    r = requests.get('https://dragoncapital.com.vn/bieu-mau-tai-lieu-quy/tai-lieu-quy/')
-    file_new = f"{re.search(r'DCDS_BC_Thang_.{6}',r.text).group()}.xlsx"
-    datetime_str  = file_new[14:20]
-
-    datetime_object = datetime.strptime(datetime_str, '%m%Y')
-    datetime_object = datetime_object + timedelta(days=31)
-    datetime_str = datetime_object.strftime('%Y/%m')
-
-    url = f'https://vfmcomvnaz.azureedge.net/dcvfmcomvn/uploads/vfm_files/report/{datetime_str}/{file_new}'
+    def get_url1():
+        r = requests.get('https://dragoncapital.com.vn/bieu-mau-tai-lieu-quy/tai-lieu-quy/')
+        file_new = f"{re.search(r'DCDS_BC_Thang_.{6}',r.text).group()}.xlsx"
+        datetime_str  = file_new[14:20]
+        datetime_object = datetime.strptime(datetime_str, '%m%Y')
+        datetime_object = datetime_object + timedelta(days=31)
+        datetime_str = datetime_object.strftime('%Y/%m')
+        url = f'https://vfmcomvnaz.azureedge.net/dcvfmcomvn/uploads/vfm_files/report/{datetime_str}/{file_new}'
+        return url, file_new
 
 
     for x in os.listdir(PATH):
         if 'DCDS' in x and '~' not in x:
-            if x == file_new:
+            now = datetime.now()
+            now_str = now.strftime("%Y%m")
+            file_time_str = f'{x[-9:-5]}{x[-11:-9]}'
+            if int(now_str) - int(file_time_str) < 2:
                 dcds_is_updated = True
+                print(x)
                 break
             else:
-                os.remove(f'{PATH}/{x}')
-                response = requests.get(url)
-                open(f'{PATH}/{file_new}', "wb").write(response.content)
-                dcds_is_updated = True
-                break
+                k = get_url1()
+                url = k[0]
+                file_new = k[1]
+                if x == file_new:
+                    dcds_is_updated = True
+                    print(file_new)
+                    break
+                else:
+                    os.remove(f'{PATH}/{x}')
+                    response = requests.get(url)
+                    open(f'{PATH}/{file_new}', "wb").write(response.content)
+                    dcds_is_updated = True
+                    print(file_new)
+                    break
+
     if dcds_is_updated == False:
         response = requests.get(url)
         open(f'{PATH}/{file_new}', "wb").write(response.content)
-    print(file_new)
+        print(file_new)
+
 check_updated_vesaf()
 check_updated_dcds()
 
@@ -86,11 +116,11 @@ for x in os.listdir(PATH):
     if 'DCDS' in x and '~' not in x:
         temp=get_symbol(x)
         percentDCDS = temp[1]
-        dfDCDS = dfDCDS.append(temp[0])
+        dfDCDS = dfDCDS._append(temp[0])
     if 'VESAF' in x and '~' not in x:
         temp=get_symbol(x)
         percentVESAF = temp[1]
-        dfVESAF = dfVESAF.append(temp[0])
+        dfVESAF = dfVESAF._append(temp[0])
 
 
 def getDataToFrame(sanCK):
