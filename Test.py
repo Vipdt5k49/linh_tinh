2db463ae3e91234cbacda638fdfd0c7d92f70869
Vipdt5k49 <117129840+Vipdt5k49@users.noreply.github.com>
2023-08-26 09:19:23 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,27 +1,7 @@
+import re
 import time
-import pandas as pd
-f = open(r'C:\Users\Admin\Desktop\PYTHON\test.txt','r')
-data = f.read()
-
-lst = data.split('PSX:V12.02.04R000:IGHL02PSX01')[1:-1]
-lst_label = []
-labels = [x for x in lst[0].split('\n') if ':' in x][:-1]
-for label in labels:
-    a = label.split(':')[0].strip()
-    lst_label.append(a)
-
-df = pd.DataFrame(columns=lst_label)
-
-
-i=1
-for a in lst:
-    print(i)
-    lst1 = [x for x in a.split('\n') if ':' in x][:-1]
-    c = []
-    for b in lst1:
-        c.append(b.split(':')[1])
-    
-    df.loc[len(df)]=c
-    i=i+1
-
-print(df)
+lst = ['abc']*10000000
+s=time.time()
+for i in lst:
+    t=re.search('b',i)
+print(time.time()-s)
